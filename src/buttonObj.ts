cbf4130da64cd07121e0828a9fa54bfa4e62c330
rkaufusi <rhettkaufusi@gmail.com>
2022-06-29 16:11:16 -0700

    example main page basic button

---
+++ src/buttonObj.ts
@@ -0,0 +1,14 @@
+const buttons = {
+    buttonOne: {
+        title: "Basic Button",
+        design: "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded",
+    },
+    buttonTwo: {
+
+    },
+    buttonThree: {
+
+    }
+}
+
+export default buttons
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,15 +1,26 @@
 import React from 'react'
+import buttonObj from '../buttonObj'
 
 type boolVal = {
     isOpen: boolean
 }
 
 const Main = ({isOpen}: boolVal) => {
-    console.log(isOpen)
+    console.log(buttonObj.buttonOne.design)
     return (
+        <>
             <div className="flex justify-center w-screen h-screen bg-slate-900">
-                <h1 className="text-white text-4xl">Main</h1>
+                <div className="flex flex-col w-auto h-auto justify-around items-center">
+                <h1 className="text-white text-4xl">{buttonObj.buttonOne.title}</h1>
+                <button className={buttonObj.buttonOne.design}>Button</button>
+                <div className="border-amber-100 border-solid border-2 w-[600px] h-64">
+                    <p className="text-lg text-amber-700 flex justify-center my-10 mx-5"><span className="text-slate-500">{`<`}</span>{`button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> Button </button>`}</p>
+                </div>
+                </div>
+
             </div>
+
+        </>
     )
 }
 
