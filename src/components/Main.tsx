53bb32e67ca3108f65b9d47bc25e7b4f8321e7f0
rkaufusi <rhettkaufusi@gmail.com>
2022-06-30 12:22:22 -0700

    added color to code snippet, abstracted button

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import buttonObj from '../buttonObj'
+import ButtonOne from './ButtonOne'
+
 
 type boolVal = {
     isOpen: boolean
@@ -10,13 +12,7 @@ const Main = ({isOpen}: boolVal) => {
     return (
         <>
             <div className="flex justify-center w-screen h-screen bg-slate-900">
-                <div className="flex flex-col w-auto h-auto justify-around items-center">
-                <h1 className="text-white text-4xl">{buttonObj.buttonOne.title}</h1>
-                <button className={buttonObj.buttonOne.design}>Button</button>
-                <div className="border-amber-100 border-solid border-2 w-[600px] h-64">
-                    <p className="text-lg text-amber-700 flex justify-center my-10 mx-5"><span className="text-slate-500">{`<`}</span>{`button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> Button </button>`}</p>
-                </div>
-                </div>
+                <ButtonOne/>
 
             </div>
 
